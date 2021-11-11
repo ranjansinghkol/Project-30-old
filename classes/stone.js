9152729215775f8b20c298105d5f7dfa063b1337
ranjansinghkol <38905611+ranjansinghkol@users.noreply.github.com>
2021-11-11 22:13:43 +0530

    Add files via upload

---
+++ classes/stone.js
@@ -0,0 +1,28 @@
+class Stone {
+  constructor(x, y, w, h) {
+    var options = {
+      restitution: 0.8
+    };
+
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    this.image = loadImage("./assets/stone.png");
+
+    World.add(world, this.body);
+  }
+
+  show() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    strokeWeight(1);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.w, this.h);
+    noStroke();
+    pop();
+  }
+}
